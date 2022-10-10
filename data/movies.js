996052b872f15fd176d96e2b810599a54593ae76
Tolya Fam <a.fam@onpoint.ru>
2022-10-10 03:25:38 +0300

    create films data

---
+++ data/movies.js
@@ -0,0 +1,92 @@
+const movies = [
+  {
+    id: "0",
+    image:
+      "https://avatars.mds.yandex.net/get-kinopoisk-image/6201401/f7d03745-15a3-4afb-a03f-0807f329f654/600x900",
+    name: "Решение уйти",
+    country: "Южная Корея",
+    genre: "Детектив",
+  },
+  {
+    id: "1",
+    image:
+      "https://avatars.mds.yandex.net/get-kinopoisk-image/4303601/dc3b228d-9e52-4e64-92b8-54f4e1f2792c/600x900",
+    name: "Пассажиры ночи",
+    country: "Франция",
+    genre: "Драма",
+  },
+  {
+    id: "2",
+    image:
+      "https://avatars.mds.yandex.net/get-kinopoisk-image/6201401/8085bef9-3275-492d-a195-f0d89d141770/600x900",
+    name: "Чудовище",
+    country: "США",
+    genre: "Ужасы",
+  },
+  {
+    id: "3",
+    image:
+      "https://avatars.mds.yandex.net/get-kinopoisk-image/6201401/608b448c-bd1e-4268-90de-1f04ab147786/600x900",
+    name: "Не говори никому",
+    country: "Дания",
+    genre: "Триллер",
+  },
+  {
+    id: "4",
+    image:
+      "https://avatars.mds.yandex.net/get-kinopoisk-image/4774061/76dfba00-a90c-4bce-90c5-78ee63c90509/600x900",
+    name: "Вышка",
+    country: "Триллер",
+    genre: "Великобритания",
+  },
+  {
+    id: "5",
+    image:
+      "https://avatars.mds.yandex.net/get-kinopoisk-image/6201401/b2fbfc01-5e08-4441-bf3e-6cd34f2a3b21/600x900",
+    name: "Исповедь",
+    country: "США",
+    genre: "Драма",
+  },
+  {
+    id: "6",
+    image:
+      "https://avatars.mds.yandex.net/get-kinopoisk-image/1898899/f51a1ae3-8869-4797-a366-9d2ccc4f355d/600x900",
+    name: "Эра выживания",
+    country: "Франция",
+    genre: "Фантастика",
+  },
+  {
+    id: "7",
+    image:
+      "https://avatars.mds.yandex.net/get-kinopoisk-image/4774061/e5998c9c-460a-462f-9607-c3f252dc1d43/600x900",
+    name: "Три тысячи лет желаний",
+    country: "Австралия",
+    genre: "Фэнтези",
+  },
+  {
+    id: "8",
+    image:
+      "https://avatars.mds.yandex.net/get-kinopoisk-image/6201401/4a82eee0-bcfc-497a-a641-2b720b2d4bbb/600x900",
+    name: "Хранитель тайн",
+    country: "Испания",
+    genre: "Триллер",
+  },
+  {
+    id: "9",
+    image:
+      "https://avatars.mds.yandex.net/get-kinopoisk-image/4774061/05b0be9d-22b0-4bf5-841d-3906d6780251/600x900",
+    name: "Безумная дорога",
+    country: "Австралия",
+    genre: "Боевик",
+  },
+  {
+    id: "10",
+    image:
+      "https://avatars.mds.yandex.net/get-kinopoisk-image/4303601/74b2483d-8295-43d5-9709-57799a686524/600x900",
+    name: "Джиперс Криперс: Возрожденный ",
+    country: "Великобритания",
+    genre: "Ужасы",
+  },
+];
+
+export default movies;
